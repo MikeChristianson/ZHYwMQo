5d31ed7a19c89e3da6af237f320a2e1c8b3e0a90
Mike Christianson <mike@codeaweso.me>
2025-02-15 13:40:01 -0700

    Initial project

---
+++ src/test/kotlin/mikec/backend/TestBackendApplication.kt
@@ -0,0 +1,9 @@
+package mikec.backend
+
+import org.springframework.boot.fromApplication
+import org.springframework.boot.with
+
+
+fun main(args: Array<String>) {
+	fromApplication<BackendApplication>().with(TestcontainersConfiguration::class).run(*args)
+}
---
+++ src/test/kotlin/mikec/backend/TestcontainersConfiguration.kt
@@ -0,0 +1,6 @@
+package mikec.backend
+
+import org.springframework.boot.test.context.TestConfiguration
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration
