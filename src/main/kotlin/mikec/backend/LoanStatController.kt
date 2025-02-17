22ea4f972649fd95327723b8bbd4bc1a1997daf6
Mike Christianson <mike@codeaweso.me>
2025-02-17 13:59:05 -0700

    impl and tests specific to summary and loan amnt direct

--- src/main/kotlin/mikec/backend/LoanStatController.kt
+++ src/main/kotlin/mikec/backend/LoanStatController.kt
@@ -1,10 +1,9 @@
 package mikec.backend
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.http.HttpEntity
 import org.springframework.http.MediaType
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,9 +12,23 @@ import org.springframework.web.bind.annotation.RestController
 class LoanStatController @Autowired constructor(
     private val repository: LoanStatRepository,
 ) {
-    @GetMapping("/summary")
-    fun statsLoanAmnt(): Any {
+    @GetMapping("/")
+    fun summary(): Any {
         val summary = repository.summarize()
+        val message = (summary as Array<*>).contentToString()
+        return message
+    }
+
+    @GetMapping("/loan_amnt")
+    fun statsLoanAmnt(): Any {
+        val summary = repository.summarizeLoanAmnt()
+        val message = (summary as Array<*>).contentToString()
+        return message
+    }
+
+    @GetMapping("/{type}")
+    fun summaryForType(@PathVariable type: String): Any {
+        val summary = repository.summarize(type)
         val message = (summary as Array<*>).contentToString()
         return message
     }
--- src/test/kotlin/mikec/backend/LoanStatControllerTest.kt
+++ src/test/kotlin/mikec/backend/LoanStatControllerTest.kt
@@ -4,10 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.http.HttpHeaders
-import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.assertj.MockMvcTester
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
@@ -16,15 +13,25 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 @AutoConfigureMockMvc
 class LoanStatControllerTest @Autowired constructor(
     private val mockMvc: MockMvc,
-    private val mockMvcTester: MockMvcTester,
-    private val loanStateController: LoanStatController,
 ) {
 
     @Test
-    fun name() {
-
+    fun `hit summary endpoint`() {
         mockMvc.perform(
-            get("/loan-stats/summary")
+            get("/loan-stats/")
+//                .header(HttpHeaders.ACCEPT, MediaType.ALL)
+        )
+            .andExpectAll(
+                status().isOk,
+                content().string("[1000, 40000, 5.32, 30.99]")
+            )
+
+    }
+
+    @Test
+    fun `hit loan_amnt endpoint`() {
+        mockMvc.perform(
+            get("/loan-stats/loan_amnt")
 //                .header(HttpHeaders.ACCEPT, MediaType.ALL)
         )
             .andExpectAll(
