98dfcca67b172c00fc870cfe899b1bfd50a78574
Mike Christianson <mike@codeaweso.me>
2025-02-17 17:03:32 -0700

    prepared statement args also make them into strings

--- src/main/kotlin/mikec/backend/LoanStatRepository.kt
+++ src/main/kotlin/mikec/backend/LoanStatRepository.kt
@@ -2,21 +2,35 @@ package mikec.backend
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.repository.query.Param
+import org.springframework.jdbc.core.PreparedStatementCreator
+import org.springframework.jdbc.core.RowCallbackHandler
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
+import java.sql.ResultSet
 
 @Repository
 class LoanStatRepository @Autowired constructor(
     private val jdbcTemplate: NamedParameterJdbcTemplate,
 ) {
 
-    fun summarize(): Map<String, Any> {
-        return jdbcTemplate.jdbcOperations.queryForMap(
-            """
-        select min(loan_amnt), max(loan_amnt), min(int_rate), max(int_rate)
+    fun summarize() {
+        return jdbcTemplate.jdbcOperations.query(PreparedStatementCreator {
+            val preparedStatement = it.prepareStatement(
+                """
+        select min(?), max(?), min(?), max(?)
         from loan_stats
     """
-        ).toMap();
+            )
+            preparedStatement.setString(1, "loan_amnt")
+            preparedStatement.setString(2, "loan_amnt")
+            preparedStatement.setString(3, "int_rate")
+            preparedStatement.setString(4, "int_rate")
+            preparedStatement
+        }, object : RowCallbackHandler {
+            override fun processRow(rs: ResultSet) {
+                rs.findColumn("fred")
+            }
+        })
     }
 
 //    @Query("""
@@ -25,11 +39,11 @@ class LoanStatRepository @Autowired constructor(
 //    """)
 //    fun summarizeLoanAmnt(): Any
 
-    fun summarize(@Param("type") type: String): Map<String, Any> {
-        val query = """
+        fun summarize(@Param("type") type: String): Map<String, Any> {
+            val query = """
         select min(:type), max(:type)
         from loan_stats
     """
-        return jdbcTemplate.queryForMap(query, mapOf("type" to type)).toMap()
+            return jdbcTemplate.queryForMap(query, mapOf("type" to type)).toMap()
+        }
     }
-}
